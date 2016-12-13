d9167386e3a36104a9263185846ea1ffece54c86
stevenk6 <stevenk6@hawaii.edu>
2016-12-12 22:52:13 -1000

    Add files via upload

---
+++ Xbee_Receiver.ino
@@ -0,0 +1,38 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial XBee(2, 3);
+//This sets pins 2 and 3 on the arduino to be used
+//as extra TX and RX. These are the pins the XBee used to send serial communication
+
+int j=0;
+void setup()
+{
+    pinMode(13,OUTPUT);
+    pinMode(12,OUTPUT);
+    XBee.begin(9600);
+    Serial.begin(9600);
+//Ensure that both receiver/transmitter are both same baud 
+}
+ 
+void loop()
+{
+  j=XBee.read();
+    if (j==1)
+   { 
+    //Turns ON
+    digitalWrite(13,HIGH);
+    digitalWrite(12,LOW);
+    Serial.print(j);
+    delay(100);
+   }
+    if (j==2)
+    {
+    //Turns OFF
+    digitalWrite(13,LOW);
+    digitalWrite(12,HIGH);
+    Serial.print(j);
+    delay(100);
+  }
+}
+
+//If no communication comes in then the it will not read 0 or 1 and would shutoff
---
+++ Xbee_Transmitter.ino
@@ -0,0 +1,36 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial XBee(2,3);
+//This sets pins 2 and 3 on the arduino to be used
+//as extra TX and RX. These are the pins the XBee used to send serial communication
+//XBee assigns the pins 2 and 3 from now on and used just as serial.Read()
+int stat;
+
+void setup()
+{
+  pinMode(12,INPUT);
+  pinMode(13,OUTPUT);
+  XBee.begin(9600);
+  Serial.begin(9600);
+}
+//Ensure that both receiver/transmitter are both same baud rate
+//High is ON
+//LOW is KILL
+void loop()
+{
+  stat=digitalRead(12);
+  //ON
+  if (stat==LOW)
+  {
+    XBee.write(1);
+    Serial.println(1);
+    delay(100);
+  }
+  else if (stat==HIGH)
+  {
+    //KILL
+    XBee.write(2);
+    Serial.println(2);
+    delay(100);
+  }
+}
